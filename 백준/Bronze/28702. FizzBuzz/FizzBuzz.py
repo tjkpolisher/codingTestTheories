aaf403bc9f2a0a6c38ed83417bbb47583f0ad048
tjkpolisher <80250150+tjkpolisher@users.noreply.github.com>
2024-06-28 14:41:59 +0900

    [Bronze I] Title: FizzBuzz, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+for i in range(3, 0, -1):
+    x = input().rstrip()
+    if x not in ['Fizz', 'Buzz', 'FizzBuzz']:
+        n = int(x) + i
+
+if n % 3 == 0 and n % 5 == 0:
+    print('FizzBuzz')
+elif n % 3 == 0:
+    print('Fizz')
+elif n % 5 == 0:
+    print('Buzz')
+else:
+    print(n)
