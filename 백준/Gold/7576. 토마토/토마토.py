81e31d20950270dc70c4f4b9f7b5ed9aaf377e54
tjkpolisher <80250150+tjkpolisher@users.noreply.github.com>
2024-07-24 16:40:35 +0900

    [Gold V] Title: 토마토, Time: 1200 ms, Memory: 98604 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -4,37 +4,36 @@ input = sys.stdin.readline
 
 M, N = map(int, input().split())
 tomato_box = []
-ripen = []
+ripen = deque()  # 큐를 여기서 바로 선언하여 초기화
+
 for i in range(N):
     line = list(map(int, input().split()))
     for j in range(M):
         if line[j] == 1:
-            ripen.append((j, i))
+            ripen.append((j, i))  # 모든 익은 토마토 위치 추가
     tomato_box.append(line)
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-
-def bfs(ripen):
-    q = deque(ripen)
-    cnt = -1
-    while q:
-        for _ in range(len(q)):
-            x, y = q.popleft()
+def bfs():
+    days = -1
+    while ripen:
+        for _ in range(len(ripen)):
+            x, y = ripen.popleft()
             for i in range(4):
                 nx = x + dx[i]
                 ny = y + dy[i]
                 if 0 <= nx < M and 0 <= ny < N and tomato_box[ny][nx] == 0:
                     tomato_box[ny][nx] = 1
-                    q.append((nx, ny))
-        cnt += 1
-    return cnt
+                    ripen.append((nx, ny))
+        days += 1
 
+    # BFS 후에 토마토가 모두 익었는지 확인
+    for row in tomato_box:
+        if 0 in row:
+            return -1
+    return days
 
-cnt = bfs(ripen)
-for i in range(N):
-    for j in range(M):
-        if tomato_box[i][j] == 0:
-            cnt = -1
-print(cnt)
+days = bfs()
+print(days)
